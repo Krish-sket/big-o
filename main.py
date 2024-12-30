deb8993a8b275a8777bfd08ecee979d2d3cc155c
Krish-sket <theworldofgamingksmvd@gmail.com>
2024-12-30 19:34:40 +0530

    big o

---
+++ main.py
@@ -0,0 +1,8 @@
+def print_number(n):
+    iteration=0
+    print("The total number entered my user is",n)
+    iteration+=1
+    print('The total iteration done by computer is ',iteration,'\n')
+n=int(input("Enter a number: "))
+print_number(n)
+print("\n with any n the time taken by the computer will not change")
